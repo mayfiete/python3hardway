f24f66aae14607fb97b58353e4e755ce9fae71f8
Terry Mayfield <terry.e.g.mayfield@gmail.com>
2021-05-18 21:57:49 -0400

    feat: updating printing.py file

--- printing.py
+++ printing.py
@@ -7,3 +7,16 @@ print(formatter.format(
     "Try your", 
     "own text here."
 ))
+
+
+# practice type string 
+days = "Mon Tue Wed Thu Fri Sat Sun" 
+months = "Jan\nFeb\nMar"
+
+print("the days: ", days) 
+print("the months ", months)
+
+# when you want to put a lot in the text
+print("""
+There's something going on here
+""")
